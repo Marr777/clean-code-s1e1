bb4c2040bb0fd12a22791dcda0ec8136d7388526
Мария <marklukanova@gmail.com>
2023-04-25 02:17:08 +0300

    fix(basic-3.1): change js according to the new classes

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement = function (taskString) {
 
   deleteButton.className = "button button_delete";
   deleteButtonImg.src = './remove.svg';
+  deleteButtonImg.className = 'button__image button__image_delete';
   deleteButton.appendChild(deleteButtonImg);
 
 
